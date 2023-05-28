9c88338c21ec8c559c9a844b1fe1ec7a9ce1463b
Nicolas-Avila <aviladanielnicolas@gmail.com>
2023-05-28 16:12:48 -0300

    punto 19

--- funciones.py
+++ funciones.py
@@ -8,6 +8,15 @@ with open('/home/oem/progrmacion-I/parcial/equipo,json','r') as data:
 def mostrar(imprimir):
     print(imprimir)
 
+def lista_vacia():
+    if len(jugadores) == 0:
+        mostrar("Lista esta vacía no puedo realizar ninguna operacion.")
+        exit()#para terminar la ejecucion del programa
+    else:
+        mostrar('BIENVENIDO!!')
+
+
+
 #1
 def all_jugadores()->None:
     '''
@@ -21,53 +30,67 @@ def un_jugador(ingreso:int)-> None:
     '''
     Recive un entero 
     La funcion muestra nombre posicion y estadisticas de la posicion ingresada
+    guarda el archivo en un csv
     '''
+    estadistica=[]
+    datos = []
     if (ingreso <= len(jugadores)):
         mostrar('{0}\n{1}\n'.format(jugadores[ingreso]['nombre'],jugadores[ingreso]['posicion']))
+        datos.append(jugadores[ingreso]['nombre'])
+        estadistica.append(jugadores[ingreso]['posicion'])
         for indice, valor in jugadores[ingreso]['estadisticas'].items():
             mostrar('{0}: {1}'.format(indice, valor))
+            estadistica.append(indice)
+            datos.append(str(valor))
 
-#4 arreglar bug
+        dato_str = ','.join(datos)
+        estadistica_str = ','.join(estadistica)
+        datos_csv =  estadistica_str + '\n'+ dato_str
+        with open ('/home/oem/progrmacion-I/parcial/csv.csv','w')as archivo:
+            archivo.writelines(datos_csv)
+            mostrar('el archivo se guardo')
+    else:
+        mostrar('ese jugador no existe')
+
+#4 
 def logros(nombre:int)->None:
     '''
     recive un entero
     la funcion funciona como un buscador y muestra los logros de los jugadores buscados 
     '''
-    encontrado = False
+    encontrado = False 
     for jugador in jugadores:
         if (re.search(nombre,jugador['nombre'])):
-            mostrar('\n{0}\nSus logros:'.format(jugador['nombre']))
+            mostrar('{0}\nSus logros:'.format(jugador['nombre']))
             for logro in jugador['logros']:
                 mostrar('{0}'.format(logro))
                 encontrado = True
-        elif encontrado == False: 
-            mostrar('Ese jugador no se pudo encontrar intente de nuevo')
-            nombre = input('ingrese un jugador a buscar: ')
-            logros(nombre)
+    if encontrado == False: 
+        mostrar('Ese jugador no se pudo encontrar intente de nuevo')
+        nombre = input('ingrese un jugador a buscar: ').capitalize()
+        logros(nombre)
 
 #5 falta la cuenta y el ordenamiento
 def promedio_puntos():
-    acumulador = 0
-    
-    for indice in jugadores:
-        acumulador = acumulador + indice['estadisticas']['promedio_puntos_por_partido']
-        acumulador = round(acumulador,2)#limita a 2 numeros con coma y redondea
-        print(acumulador)
+    pass
 
-#6 bug del 4
-def salon_fama(nombre):
+#6 
+def salon_fama(nombre:str)->None:
+    '''
+    recive un string
+    averigua si el jugador pertenece al salon de la fama
+    '''
     encontrado = False
-    ave = 'Miembro del Salon de la Fama del Baloncesto'
+    salon = 'Miembro del Salon de la Fama del Baloncesto'
     for jugador in jugadores:
         if (re.match(nombre,jugador['nombre'])):
-                print('encontre')
-                if ave in jugador['logros']:
+                if salon in jugador['logros']:
                     encontrado=True
-                    mostrar('si pertenece al salon de la fama')
+                    mostrar('{0} si pertenece al salon de la fama'.format(jugador['nombre']))
                 elif encontrado==False:
-                    mostrar('no pertenece al salon de la fama')
+                    mostrar('{0} no pertenece al salon de la fama'.format(jugador['nombre']))
 
-#7#8#9
+#7#8#9#13#14
 def estadistica_mayor(key:str)->None:
     '''
     recive un string
@@ -83,9 +106,14 @@ def estadistica_mayor(key:str)->None:
     key = key.replace('_',' ')
     mostrar('la mayor cantidad de {0} la tiene el jugador {2} con {1} {0}'.format(key,maximo,jugador))
 
-#10
-def jugador_rendimiento(key:str,ingreso:int):
+#10#11#12#15
+def jugador_rendimiento(key:str,ingreso:int)->None:
+    '''
+    recibe un string y un int 
+    averigua los jugadores con mayor valor ingresado 
+    '''
     jugador = []
+    
     for indice in jugadores:
         for i , valor in indice['estadisticas'].items():
             if i == key:
@@ -95,4 +123,38 @@ def jugador_rendimiento(key:str,ingreso:int):
     mostrar('los jugadores que tiene mas {0} del valor ingresado ({1}) son:'.format(key,ingreso))
     for nombre in jugador:
         print(nombre, end=' ')
+
+#16
+def puntos_totales()->None:
+    '''
+    calcula el promedio de los puntos por partidos sacando al peor jugador
+    '''
+    minimo = 0
+    acumulador = 0
+    cont = 0
+    for indice in jugadores:
+        for i,valor in indice['estadisticas'].items():
+            if i == 'promedio_puntos_por_partido':
+                acumulador= acumulador + valor
+                cont += 1
+                if minimo == 0 or minimo > valor:
+                    
+                    minimo = valor
+    acumulador_total = acumulador - minimo
+    cont -=1
+    porcentaje = acumulador_total / cont
+    porcentaje= round(porcentaje,2)
+    print(cont)
+    mostrar('la cantidad de puntos por partido sacando al peor jugador es de {0}'.format(porcentaje))
+
+#17
+def mayor_logros()->None:
+    mas_logros = 0
+    for indice in jugadores:
+        logros=len(indice['logros'])
+        if mas_logros < logros:
+            jugador = indice['nombre']
+            mas_logros = logros
+    
+    mostrar('el jugador con mas logros es {0} con {1} logros'.format(jugador, mas_logros))
 
--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import re 
-import json 
 from funciones import (
+    lista_vacia,
     all_jugadores,
     un_jugador,
     logros,
     promedio_puntos,
     salon_fama,
     estadistica_mayor,
-    jugador_rendimiento
+    jugador_rendimiento,
+    puntos_totales,
+    mayor_logros,
+    
 
     )
-
+lista_vacia()
 #menu
 
 menu = int(input('ingrese una opcion: '))
@@ -24,31 +27,74 @@ match (menu):
         ingreso = int(input('ingrese el numero del jugador: '))
         un_jugador(ingreso)
 
-    case 3:
-        pass
 
-    case 4:
+    case 3:
         nombre = input('ingrese un jugador a buscar: ').capitalize()
         logros(nombre)
 
-    case 5:
+    case 4:
+        #no terminado 
         promedio_puntos()
 
-    case 6:
+    case 5:
         nombre = input('ingrese un jugador a buscar: ')
         salon_fama(nombre.capitalize())
 
-    case 7:
+    case 6:
         estadistica_mayor('rebotes_totales')
 
-    case 8:
+    case 7:
         estadistica_mayor('porcentaje_tiros_de_campo')
 
-    case 9:
+    case 8:
         estadistica_mayor('asistencias_totales')
 
+    case 9:
+        valor = input('ingrese un valor: ')
+        if re.match('[0-9]',valor):
+            jugador_rendimiento('promedio_puntos_por_partido',int(valor))
+
     case 10:
-        valor = int(input('ingrese un valor: '))
-        jugador_rendimiento('promedio_puntos_por_partido',valor)
+        valor = input('ingrese un valor: ')
+        if re.match('[0-9]',valor):
+            jugador_rendimiento('promedio_rebotes_por_partido',int(valor))
+        
+    case 11:
+        valor = input('ingrese un valor: ')
+        if re.match('[0-9]',valor):
+            jugador_rendimiento('promedio_asistencias_por_partido',int(valor))
+    
+    case 12:
+        estadistica_mayor('robos_totales')
+
+    case 13:
+        estadistica_mayor('bloqueos_totale')
+
+    case 14:
+        valor = input('ingrese un valor: ')
+        if re.match('[0-9]',valor):
+            jugador_rendimiento('porcentaje_tiros_libres',int(valor))
+        else:
+            print('no es un numero')
+
+    case 15:
+        puntos_totales()
+
+    case 16:
+        mayor_logros()
+
+    case 17:
+        valor = input('ingrese un valor: ')
+        if re.match('[0-9]',valor):
+            jugador_rendimiento('porcentaje_tiros_triples',int(valor))
+        else:
+            print('no es un numero')
+
+    case 18:
+        estadistica_mayor('temporadas')
+
+    case 19:
+        pass
+
     case _:
-        print('tonto')
+        print('Oh no °-° \nesa opcion no existe')
